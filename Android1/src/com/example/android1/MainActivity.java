2eae33b3e2bf07e98844c592f7f96f4305a2ac46
kennyhouse <kennyhouse@gmail.com>
2013-06-09 19:43:55 -0700

    more change.

--- Android1/src/com/example/android1/MainActivity.java
+++ Android1/src/com/example/android1/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends Activity {
 
 	//comment
 	
+	//more comment
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
